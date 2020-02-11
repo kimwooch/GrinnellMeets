e4a7c8ee7d8a0eefdd3b7b04387bf223b980a1a4
choijong@grinnell.edu <choijong@grinnell.edu>
2020-02-11 14:19:28 -0600

    logo change

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -17,7 +17,7 @@ function NavBar() {
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
-        <a href="/">GC Clubs</a>
+        <a href="/">GC Meets</a>
 
       </div>
       <div className="menu__container">
