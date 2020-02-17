424db1253891acf3018f2144651f425b63da94d3
choijong@grinnell.edu <choijong@grinnell.edu>
2020-02-16 21:47:30 -0600

    product image

--- client/src/components/views/DetailProductPage/Sections/ProductImage.js
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -10,8 +10,8 @@ function ProductImage(props) {
 
             props.detail.images && props.detail.images.map(item => {
                 images.push({
-                    original: `http://localhost:5000/${item}`,
-                    thumbnail: `http://localhost:5000/${item}`
+                    original: `/${item}`,
+                    thumbnail: `/${item}`
                 })
             })
             setImages(images)
